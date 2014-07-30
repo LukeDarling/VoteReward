2bd9567cf8ddd406f52d248dc2c7fd2990ed666d
Luke Darling <HTMLguy7@gmail.com>
2014-07-29 20:14:45 -0400

    Create Main.php

---
+++ src/LDX/VoteReward/Main.php
@@ -0,0 +1,67 @@
+<?php
+namespace LDX\VoteReward;
+use LDX\VoteReward\Query;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use pocketmine\item\Item;
+class Main extends PluginBase {
+  public function onEnable() {
+    if(!file_exists($this->getDataFolder() . "config.yml")) {
+      @mkdir($this->getDataFolder());
+      file_put_contents($this->getDataFolder() . "config.yml",$this->getResource("config.yml"));
+    }
+    $c = yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
+    $num = 0;
+    $this->key = $c["API-Key"];
+    $this->url = $c["Vote-URL"];
+    $rewards = $c["Rewards"];
+    foreach($rewards["Items"] as $i) {
+      $r = explode(":",$i);
+      $this->items[$num] = new Item($r[0],$r[1],$r[2]);
+      $num++;
+    }
+    $num = 0;
+    foreach($rewards["Commands"] as $i) {
+      $this->commands[$num] = $i;
+      $num++;
+    }
+  }
+  public function onCommand(CommandSender $p,Command $cmd,$label,array $args) {
+    if(!($p instanceof Player)) {
+      $p->sendMessage("Command must be used in-game.");
+      return true;
+    }
+    if($p->hasPermission("votereward") || $p->hasPermission("votereward.vote")) {
+      $query = new Query($this,$p,"http://minecraftpocket-servers.com/api/?object=votes&element=claim&key=" . $this->key . "&username=" . $p->getName(),true);
+    } else {
+      $p->sendMessage("You do not have permission to vote.");
+    }
+    return true;
+  }
+  public function give($p,$s) {
+    $s = 1; // DEBUG
+    if($s == "0") {
+      $p->sendMessage("You haven't voted yet!\n" . $this->url . "\nVote now for cool rewards!");
+    } else if($s == "1") {
+      $query = new Query($this,$p,"http://minecraftpocket-servers.com/api/?action=post&object=votes&element=claim&key=" . $this->key . "&username=" . $p->getName(),false);
+      foreach($this->items as $i) {
+        $p->getInventory()->addItem($i);
+      }
+      foreach($this->commands as $i) {
+        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),$i);
+      }
+    } else if($s == "2") {
+      $p->sendMessage("You've already voted today! Come back tomorrow to vote again.");
+    } else {
+      $this->getLogger()->warning(TextFormat::RED . "Error fetching vote status! Are you hosting your server on a mobile device, or is your Internet out?");
+      $p->sendMessage("[VoteReward] Error fetching vote status!");
+    }
+  }
+  public function onDisable() { }
+}
+?>
