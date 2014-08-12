981ca2facdb24a95b257959d90c68ffccfe08667
Luke Darling <HTMLguy7@gmail.com>
2014-08-12 17:45:42 -0400

    Update QueryTask.php

--- src/LDX/VoteReward/QueryTask.php
+++ src/LDX/VoteReward/QueryTask.php
@@ -1,24 +1,23 @@
 <?php
-
 use pocketmine\Player;
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\Server;
-
-class QueryTask extends AsyncTask{
-    private $url, $data, $p, $ret;
-    function __construct($url, $p, $ret){
+class QueryTask extends AsyncTask {
+    function __construct($url,$p,$r) {
         $this->url = $url;
         $this->p = $p;
-        $this->ret = $ret;
+        $this->r = $r;
     }
-    public function onRun(){
+    public function onRun() {
         $this->data = file_get_contents($this->url);
     }
-    public function onCompletion(Server $server){
-        if($this->ret){
-            if(($p = $server->getPlayer($this->p)) instanceof Player){
-                $server->getPluginManager()->getPlugin("VoteReward")->give($p, $this->data);
+    public function onCompletion(Server $server) {
+        if($this->r) {
+            $p = $server->getPlayer($this->p);
+            if($p instanceof Player) {
+                $server->getPluginManager()->getPlugin("VoteReward")->give($p,$this->data);
             }
         }
     }
 }
+?>
