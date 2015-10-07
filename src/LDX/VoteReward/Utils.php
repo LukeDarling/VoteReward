8d9da5e62d5fcc89309a3e384cb3adc09cbf6826
Luke Darling <luke@lukedarling.net>
2015-10-07 17:30:20 -0400

    added Utils::hasPermission() static method

--- src/LDX/VoteReward/Utils.php
+++ src/LDX/VoteReward/Utils.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace LDX\VoteReward;
+
+use pocketmine\Player;
+
+class Utils {
+
+  public static function hasPermission($player,$permission) {
+    $base = "";
+    $nodes = explode(".",$permission);
+    foreach($nodes as $key => $node) {
+      $seperator = $key == 0 ? "" : ".";
+      $base = "$base$seperator$node";
+      if($player->hasPermission($base)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+}
