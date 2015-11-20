46e88d49c919f8cff1a68c2361731bd206a249cb
Luke Darling <luke@lukedarling.net>
2015-11-20 01:29:42 -0500

    Added rewardPlayer method

--- src/LDX/VoteReward/Main.php
+++ src/LDX/VoteReward/Main.php
@@ -28,8 +28,8 @@ class Main extends PluginBase {
     $this->message = $config["Message"];
     $this->items = [];
     foreach($config["Items"] as $i) {
-      $r = explode(":",$i);
-      $this->items[] = new Item($r[0],$r[1],$r[2]);
+      $r = explode(":", $i);
+      $this->items[] = new Item($r[0], $r[1], $r[2]);
     }
     $this->commands = [];
     foreach($config["Commands"] as $i) {
@@ -43,6 +43,49 @@ class Main extends PluginBase {
       $clones[] = clone $item;
     }
     return $clones;
+  }
+
+  public function rewardPlayer(Player $player, int $multiplier = 1) {
+    foreach($this->getItems() as $item) {
+      $item->setCount($item->getCount() * $multiplier);
+      $player->addItem($item);
+    }
+    $commands = $this->commands;
+    foreach($commands as $command) {
+      preg_replace_callback("/(\\\&|\&)[0-9a-fk-or]/", function(array $matches) {
+        return str_replace("\\§", "&", str_replace("&", "§", $matches[0]));
+      }, $command);
+      $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace(array(
+        "{USERNAME}",
+        "{NICKNAME}",
+        "{X}",
+        "{Y}",
+        "{Z}"
+      ), array(
+        $player->getName(),
+        $player->getDisplayName(),
+        $player->getX(),
+        $player->getY(),
+        $player->getZ()
+      ), $command));
+    }
+    if(trim($message) != "") {
+      $message = $this->message;
+      preg_replace_callback("/(\\\&|\&)[0-9a-fk-or]/", function(array $matches) {
+        return str_replace("\\§", "&", str_replace("&", "§", $matches[0]));
+      }, $message);
+      $message = str_replace(array(
+        "{USERNAME}",
+        "{NICKNAME}"
+      ), array(
+        $player->getName(),
+        $player->getDisplayName()
+      ), $message);
+      foreach($this->getServer()->getOnlinePlayers() as $p) {
+        $p->sendMessage($message);
+      }
+      $this->getServer()->getLogger()->info($message);
+    }
   }
 
 }
